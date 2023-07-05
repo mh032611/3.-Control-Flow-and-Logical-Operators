a94c79f7bd649876f6de6aca2766e49d9dc3b4c5
mh032611 <mh032611@gmail.com>
2023-07-05 03:08:17 -0500

    midlife crisis rollercoaster

---
+++ Midlife Crisis Rollercoaster.py
@@ -0,0 +1,26 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height > 120:
+  age = int(input("What is your age?"))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5.")
+  elif age < 18:
+    bill = 7
+    print("Youth tickets are $7.")
+  elif age >= 18 and age < 45:
+    bill = 12
+    print("Adult tickets are $12.")
+  else:
+    bill = 0
+  want_photos = input("Do you want photos? Enter 'Y' or 'N'.")
+  if want_photos == "Y":
+    bill += 3
+else:
+  print("You Cant Ride, need to be at least 120 cm Tall")
+
+statement = f"The total bill is ${bill}"
+
+print(statement)
